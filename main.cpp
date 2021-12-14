ebbb27b89bf9d13df78677da140e3dab0393a2f8
VampItUp <thevampitup@gmail.com>
2021-12-14 17:47:30 -0600

    add

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
-int main() {}
+#include <iostream>
+
+struct Hopper
+{
+
+};
+
+struct HeatSource
+{
+
+};
+
+struct WaterReservoir
+{
+
+};
+
+struct Grinder
+{
+
+};
+
+struct CupReceptacle
+{
+
+};
+
+struct CoffeeMaker : Hopper, HeatSource , WaterReservoir , Grinder , CupReceptacle
+{
+    void make()
+    {
+
+    }
+};
+
+
+int main() 
+{
+    CoffeeMaker *c = new CoffeeMaker();
+    c.make();
+}
